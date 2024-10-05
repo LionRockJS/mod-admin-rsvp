7d12d2e13e568d8f24112e6fb53020d52f34893c
Colin Leung <colin@cowise.co>
2024-10-05 13:49:46 +0800

    chore: update router

--- routes.mjs
+++ routes.mjs
@@ -1,4 +1,4 @@
-import { RouteList } from '@lionrockjs/central';
+import { RouteList } from '@lionrockjs/router';
 
 RouteList.add('/admin/rsvp', 'controller/admin/RSVP');
 RouteList.add('/admin/rsvp/send/:id', 'controller/admin/RSVP', 'send_email');
