d7d6408ae0cbf5068b7f666e7ab148193a441662
Colin Leung <colin@cowise.co>
2024-10-13 11:38:13 +0800

    feat: v2 filename and configs

--- init.mjs
+++ init.mjs
@@ -1,6 +1,1 @@
-import { Central } from '@lionrockjs/central';
-
-await Central.initConfig(new Map([
-  ['edm', await import('./config/edm.mjs')],
-  ['rsvp', await import('./config/rsvp.mjs')],
-]));
+export default {}
